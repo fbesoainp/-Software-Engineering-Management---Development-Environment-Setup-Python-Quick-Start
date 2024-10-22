8f49408c92c64028cd45b9219291e7599a04ba83
fbesoainp <chachibesoain1@gmail.com>
2024-10-21 18:06:00 -0700

    ✨ Add more query examples

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cellxgene_census
+import tiledbsoma
 
 with cellxgene_census.open_soma() as census:
 
@@ -15,3 +16,36 @@ with cellxgene_census.open_soma() as census:
     cell_metadata = cell_metadata.to_pandas()
 
     print(cell_metadata)
+
+
+with cellxgene_census.open_soma() as census:
+    adata = cellxgene_census.get_anndata(
+        census = census,
+        organism = "Homo sapiens",
+        var_value_filter = "feature_id in ['ENSG00000161798', 'ENSG00000188229']",
+        obs_value_filter = "sex == 'female' and cell_type in ['microglial cell', 'neuron'] and assay == '10x 3'",
+        column_names = {"obs": ["assay", "cell_type", "tissue", "tissue_general", "suspension_type", "disease"]},
+    )
+
+    adata = adata[:100, :]
+
+    print(adata)
+
+
+with cellxgene_census.open_soma() as census:
+
+    human = census["census_data"]["homo_sapiens"]
+    query = human.axis_query(
+       measurement_name = "RNA",
+       obs_query = tiledbsoma.AxisQuery(
+           value_filter = "sex == 'female' and cell_type in ['microglial cell', 'neuron']"
+       )
+    )
+
+    iterator = query.X("raw").tables()
+
+    # Get an iterative slice as pyarrow.Table
+    raw_slice = next(iterator)
+    print(raw_slice)
+
+    query.close()
